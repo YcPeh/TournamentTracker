9ee8365d1ed528f3d6c7899f37b32ac67a186f27
YcPeh <104683280+YcPeh@users.noreply.github.com>
2023-11-07 20:49:15 +0800

    self added if statement in TournamentViewerForm.cs to prevent null exception. Until Tournament Viewer Part 1 (19.07.33))

--- TrackerUI/TournamentDashboardForm.cs
+++ TrackerUI/TournamentDashboardForm.cs
@@ -26,5 +26,12 @@ namespace TrackerUI
             CreateTournamentForm frm = new CreateTournamentForm();
             frm.Show();
         }
+
+        private void loadTournamentButton_Click(object sender, System.EventArgs e)
+        {
+            TournamentModel tm = (TournamentModel)loadExistingTournamentDropDown.SelectedItem;
+            TournamentViewerForm frm = new TournamentViewerForm(tm);
+            frm.Show();
+        }
     }
 }
